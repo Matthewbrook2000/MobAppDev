25294b103a316943828739e818c380e7ab33d1d1
Matt Brook <46922873+Matthewbrook2000@users.noreply.github.com>
2020-01-28 10:45:47 +0000

    week3 maps done

---
+++ maps/app/src/main/java/com/example/maps/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.maps;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import android.widget.Button;
+        import android.view.View.OnClickListener;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.TextView;
+        import android.widget.EditText;
+
+        import org.osmdroid.config.Configuration;
+        import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+        import org.osmdroid.util.GeoPoint;
+        import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    MapView mv;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        setContentView(R.layout.activity_main);
+
+        mv = findViewById(R.id.map1);
+
+        mv.setMultiTouchControls(true);
+        mv.getController().setZoom(16.0);
+        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        TextView tv = (TextView)findViewById(R.id.tv1);
+        EditText et = (EditText)findViewById(R.id.et1);
+        TextView tv2 = (TextView)findViewById(R.id.tv2);
+        EditText et2 = (EditText)findViewById(R.id.et2);
+
+        try
+//        {
+          mv.setX(Float.parseFloat(et.getText().toString()));
+        }
+        catch (Exception e)‏
+        {
+            System.out.println("exception occured");
+        }
+
+        mv.setY(Float.parseFloat(et2.getText().toString()));
+
+        //double feet = Double.parseDouble(et.geText().toString());
+        //double metres = feet*0.305;
+        //tv.setText("In metres that is: " + metres);
+    }
+}
